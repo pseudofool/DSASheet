2349204b2ed0a8c8f2d53c666ae3407510599dc1
Sachin Kumar <42477579+pseudofool@users.noreply.github.com>
2022-06-07 16:01:04 +0530

    Added arrays folder

---
+++ Arrays/min_and_max_element.cpp
@@ -0,0 +1,16 @@
+pair<long long, long long> getMinMax(long long a[], int n) {
+    long long min = INT_MAX;
+    long long max = INT_MIN;
+    for (int i=0; i<n; i++){
+        if (a[i] > max){
+            max = a[i];
+        }
+        if (a[i] < min){
+            min = a[i];
+        }
+    }
+    pair<long long, long long> p;
+    p.first = min;
+    p.second = max;
+    return p;
+}
