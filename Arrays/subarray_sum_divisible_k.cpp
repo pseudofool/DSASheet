4a22e354700ef059a75c4b1a052168cd28ed0ef5
Sachin Kumar <42477579+pseudofool@users.noreply.github.com>
2022-06-12 16:58:02 +0530

    Longest subarray with sum divisible by K

---
+++ Arrays/subarray_sum_divisible_k.cpp
@@ -0,0 +1,33 @@
+// link - https://practice.geeksforgeeks.org/problems/longest-subarray-with-sum-divisible-by-k1259/1/#
+/*
+    Brute Force:
+    Time comlexity - O(n^2)
+    Space complexity - O(2^n)
+*/
+/*
+    optimal
+    Time complexity - O(n) 
+    Space Complexity - O(n)
+*/
+int longSubarrWthSumDivByK(int arr[], int n, int k)
+{
+    int len = 0;
+    unordered_map<int,int> mp;
+    int sum = 0;
+    int rem = 0;
+    mp.insert({rem,-1});
+    for(int i=0; i<n; i++){
+        sum += arr[i];
+        rem = sum%k;
+        if(rem<0){
+            rem += k;
+        }
+        if(mp.count(rem)>0){
+            int currLen = i-mp[rem];
+            len = max(currLen, len);
+        }else{
+            mp.insert({rem,i});
+        }
+    }
+    return len;
+}
